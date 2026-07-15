a687705cecd881f2f0665d6b5832325db6975f70
Chong Sin Yan <sinyanchong4@gmail.com>
2026-07-15 23:04:11 +0800

    Final Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,8 +17,8 @@ public class playerController : MonoBehaviour
     InputAction look,jump,move;
     Rigidbody rb;
     // private float sensitivity=1f;
-    private float dist=8f;
-    private float jumpforce=6f;
+    private float dist=4f;
+    private float jumpforce=3f;
     private int jumpCount = 0;
     public int maxJump = 1;
     // bool touchGround = false;
